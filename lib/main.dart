fb5300e69e0e668b27268a9b14d21e9ff0ebb1eb
patrick shyaka <shyakap48@gmail.com>
2024-02-12 15:31:04 +0200

    page  main

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+  
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
@@ -34,6 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       drawer: HomeScreen(),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
